b358547f88c40b03273d97663eee65a779530b8f
soumyadip-halder <soumyadip.halder@gmail.com>
2022-01-02 14:46:22 +0530

    Login page modification

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,35 +1,43 @@
-import { AppBar, Grid, Toolbar } from "@material-ui/core";
-import GoogleLogin from "react-google-login";
-import { useHistory } from "react-router-dom";
-import { LogoMin } from "../../Logos";
-import { loginUser } from "../../redux/Actions/Login";
-import { connect } from "react-redux";
-import { useEffect } from "react";
-import { useCallback } from "react";
-import Config from "../../config/Config";
-import { useStyles } from "./Styles";
-import LoadingComponent from "../../components/LoadingComponent/LoadingComponent";
+import {
+  AppBar,
+  Grid,
+  Toolbar,
+  useTheme,
+  useMediaQuery,
+} from '@material-ui/core'
+import GoogleLogin from 'react-google-login'
+import { useHistory } from 'react-router-dom'
+import { Logo } from '../../Logos'
+import { loginUser } from '../../redux/Actions/Login'
+import { connect } from 'react-redux'
+import { useEffect } from 'react'
+import { useCallback } from 'react'
+import Config from '../../config/Config'
+import { useStyles } from './Styles'
+import LoadingComponent from '../../components/LoadingComponent/LoadingComponent'
 
 function Login(props: any) {
-  const { loginUser, user, isLoading, errorMessage } = props;
-  const classes = useStyles();
-  let history = useHistory();
-  const { GOOGLE_CLIENT_ID } = Config;
+  const { loginUser, user, isLoading, errorMessage } = props
+  const theme = useTheme()
+  const active = useMediaQuery(theme.breakpoints.down(500))
+  const classes = useStyles()
+  let history = useHistory()
+  const { GOOGLE_CLIENT_ID } = Config
   const responseGoogle = useCallback(
     (response: any) => {
-      console.log("starting response");
-      if ("tokenId" in response) {
+      console.log('starting response')
+      if ('tokenId' in response) {
         if (response && response.tokenId) {
-          localStorage.setItem("_Gresponse", JSON.stringify(response));
+          localStorage.setItem('_Gresponse', JSON.stringify(response))
           const idToken =
-            response && response.tokenObj && response.tokenObj.id_token;
-          console.log(idToken);
-          loginUser(idToken);
+            response && response.tokenObj && response.tokenObj.id_token
+          console.log(idToken)
+          loginUser(idToken)
         }
       }
     },
     [loginUser]
-  );
+  )
   // useEffect(() => {
   //   console.log("before enter")
   //   if(localStorage.getItem('_Gresponse') && localStorage.getItem('_GresponseV2')) {
@@ -42,14 +50,14 @@ function Login(props: any) {
   // }, [responseGoogle, history])
   useEffect(() => {
     if (user) {
-      history.push("/commercial-webapp/dashboard");
+      history.push('/commercial-webapp/dashboard')
     }
-  }, [user, history]);
+  }, [user, history])
 
   const responseGoogleerror = (error: any) => {
-    console.log(error);
-    history.push("/login");
-  };
+    console.log(error)
+    history.push('/login')
+  }
   // useEffect(() => {
   //   if (errorMessage) {
   //     let errorCode = ServiceResponse.getMessage("login", "serviceUnavailable");
@@ -110,32 +118,54 @@ function Login(props: any) {
   return (
     <>
       <div style={{ flexGrow: 1 }}>
-        <AppBar style={{ alignItems: "center", justifyContent: "center" }}>
-          <Toolbar style={{ alignItems: "center", justifyContent: "center" }}>
+        <AppBar
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Toolbar
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: !active ? '40vh' : '20vh',
+            }}
+          >
             <Grid container className={classes.gridContainer}>
               <Grid item className={classes.logo} xs={4} xl={4} md={4} lg={4}>
-                <LogoMin />
+                <Logo height={500} />
               </Grid>
             </Grid>
           </Toolbar>
         </AppBar>
-        <div className={classes.height} />
+        <div
+          className={classes.height}
+          style={{ height: !active ? '30vh' : '10vh' }}
+        />
         <div
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
+            display: 'flex',
+            flexGrow: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
             marginTop: 10,
+            height: '50vh',
           }}
         >
-          <h1>Sign in with Google</h1>
+          <div className={'display6'} style={{ color: '#004e37' }}>
+            Commercial Web Application
+          </div>
+          <br />
+          {/* <div className="subtitle2" style={{ color: '#004e37' }}>
+            Sign in with Google
+          </div> */}
           <GoogleLogin
             clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
+            buttonText="Signin with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogleerror}
-            cookiePolicy={"single_host_origin"}
+            cookiePolicy={'single_host_origin'}
             style={{ flex: 1 }}
             autoLoad={false}
           />
@@ -143,10 +173,10 @@ function Login(props: any) {
         <div
           className={classes.error}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
             marginTop: 10,
           }}
         >
@@ -155,7 +185,7 @@ function Login(props: any) {
         <LoadingComponent showLoader={isLoading} />
       </div>
     </>
-  );
+  )
 }
 
 const mapStateToProps = (state: any) => {
@@ -164,13 +194,13 @@ const mapStateToProps = (state: any) => {
     error: state.loginReducer.error,
     errorMessage: state.loginReducer.errorMessage,
     isLoading: state.loginReducer.isLoading,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
     loginUser: (idToken: any) => dispatch(loginUser(idToken)),
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
